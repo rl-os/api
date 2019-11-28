95f66653a6702083531cc2597077e71254c1d04e
Kudryavtsev Kirill <kudryavtsev@nppsatek.ru>
2019-11-28 15:48:08 +0300

    fix(scripts/install): log

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,6 +7,9 @@ echo "  >  Checking if there is any missing dependencies..."
 go mod download
 
 echo "  >  Cleaning build cache"
+go clean
+
+echo "  >  Installing unnessosory packages"
 go get -v golang.org/x/lint/golint
 go get -v github.com/fzipp/gocyclo
 go get -v golang.org/x/tools/cmd/goimports
