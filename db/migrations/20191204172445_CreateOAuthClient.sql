4c3c1626d062c6ee8e2d0eab912472dbc56d6f19
Kirill Kudryavtsev <deissh@yandex.ru>
2019-12-04 20:56:55 +0300

    feat: add oauth_client service and rework oauth_token

---
+++ db/migrations/20191204172445_CreateOAuthClient.sql
@@ -0,0 +1,24 @@
+-- migrate:up
+create table oauth_client
+(
+    id            serial                              not null
+        constraint oauth_token_pk
+            primary key,
+    user_id       integer                             not null,
+    name  varchar                             not null,
+    secret  varchar                             not null,
+    redirect varchar                             not null,
+    revoked       boolean default false                            not null,
+    created_at    timestamp default CURRENT_TIMESTAMP not null
+);
+public
+alter table oauth_client
+    owner to postgres;
+
+create unique index oauth_client_id_uindex
+    on oauth_client (id);
+
+-- migrate:down
+
+drop index oauth_client_id_uindex;
+drop table oauth_client;
