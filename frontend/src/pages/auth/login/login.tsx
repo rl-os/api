4d2d4c1cf1720a04d9ec4a35c9cec9e72ba2c0d0
deissh <deissh@yandex.ru>
2020-06-03 11:35:21 +0300

    feat(frontend): login page

--- frontend/src/pages/auth/login/login.tsx
+++ frontend/src/pages/auth/login/login.tsx
@@ -3,6 +3,7 @@ import classNames from "classnames";
 import { observer } from 'mobx-react-lite';
 
 import styles from './login.module.scss';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
   return <div className={classNames(styles.root)}>
@@ -13,19 +14,44 @@ const Login = () => {
     <div className="flex-row-fluid d-flex flex-column justify-content-center position-relative overflow-hidden p-7 ml-auto mr-auto">
       <div className="d-flex flex-column-fluid flex-center mt-6 mt-lg-0">
         <form>
-          <div className="pt-lg-40 mt-lg-10 pb-15">
-            <h3 className="font-weight-bolder text-dark display5">Добро пожаловать на Risu.Life</h3>
+          <div className={classNames(styles.title)}>
+            <h3 className="font-weight-bolder text-dark display5">
+              Добро пожаловать на Risu.Life
+            </h3>
             <span className="text-muted font-weight-bold font-size-h4">
-            Нет аккаунта? <a href="#" className="text-primary font-weight-bolder">Регистрация</a>
-          </span>
+              Нет аккаунта? <Link to="/auth/signup" className="text-primary font-weight-bolder">
+                Регистрация
+              </Link>
+            </span>
           </div>
+
+          <div className="form-group fv-plugins-icon-container">
+            <label className="font-size-h6 font-weight-bolder text-dark">Логин</label>
+            <input className="form-control form-control-solid h-auto py-7 px-6 rounded-lg" type="text"/>
+          </div>
+
+          <div className="form-group fv-plugins-icon-container">
+            <label className="font-size-h6 font-weight-bolder text-dark">Пароль</label>
+            <input className="form-control form-control-solid h-auto py-7 px-6 rounded-lg" type="password"/>
+          </div>
+
+          <button className={classNames(styles.btn, "btn btn-primary")}>
+            Войти
+          </button>
+
         </form>
       </div>
 
-      <div className="d-flex justify-content-lg-start justify-content-center flex-column-fluid align-items-end pb-2 pt-lg-0">
-        <a href="#" className="text-primary font-weight-bolder font-size-h5">Правила</a>
-        <a href="#" className="text-primary ml-3 font-weight-bolder font-size-h5">Контакты</a>
-        <a href="#" className="text-primary ml-3 font-weight-bolder font-size-h5">О проекте</a>
+      <div className="d-flex justify-content-lg-start justify-content-center align-items-end pb-2 pt-lg-0">
+        <a href="#" className="text-primary font-weight-bolder font-size-h5">
+          Правила
+        </a>
+        <a href="#" className="text-primary ml-3 font-weight-bolder font-size-h5">
+          Контакты
+        </a>
+        <a href="#" className="text-primary ml-3 font-weight-bolder font-size-h5">
+          О проекте
+        </a>
       </div>
     </div>
   </div>;
