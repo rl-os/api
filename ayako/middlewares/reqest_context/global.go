6b703f357da4b65beff9672cc1a9c9784789d9e3
deissh <deissh@yandex.ru>
2020-06-30 10:52:19 +0300

    fix(ayako): growing context

--- ayako/middlewares/reqest_context/global.go
+++ ayako/middlewares/reqest_context/global.go
@@ -12,19 +12,24 @@ func GlobalMiddleware(ctx context.Context) echo.MiddlewareFunc {
 		return func(c echo.Context) error {
 			req := c.Request()
 
+			// context is growing very quickly
+			// due to the fact that it is used directly
+			// so create a copy
+			copyCtx := ctx
+
 			if userId, ok := c.Get("current_user_id").(uint); ok {
-				ctx = myctx.Pipe(ctx, myctx.SetUserID(userId))
+				copyCtx = myctx.Pipe(copyCtx, myctx.SetUserID(userId))
 			}
 
 			if token, ok := c.Get("current_user_token").(string); ok {
-				ctx = myctx.Pipe(ctx, myctx.SetUserToken(token))
+				copyCtx = myctx.Pipe(copyCtx, myctx.SetUserToken(token))
 			}
 
 			if id := req.Header.Get(echo.HeaderXRequestID); id != "" {
-				ctx = myctx.Pipe(ctx, myctx.SetRequestID(id))
+				copyCtx = myctx.Pipe(copyCtx, myctx.SetRequestID(id))
 			}
 
-			c.Set("context", ctx)
+			c.Set("context", copyCtx)
 
 			return next(c)
 		}
