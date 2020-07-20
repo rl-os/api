480a4cf743ecabfecb2ed5f256b28c6e37ccec22
deissh <deissh@yandex.ru>
2020-07-20 17:24:05 +0300

    feat(frontend): auto refresh token if expired (+ from localstorage)

--- frontend/src/store/auth.ts
+++ frontend/src/store/auth.ts
@@ -6,6 +6,7 @@ import { KJUR } from 'jsrsasign';
 import { JWTAccessToken } from "../common/interfaces/access_token";
 
 const LOCALSTORAGE_KEY = 'user_auth';
+const TIME_BEFORE_ISSUE_TOKEN = 2 * 60; // 2min
 
 export class Auth {
   @observable
@@ -39,6 +40,7 @@ export class Auth {
     }
 
     try {
+      await this.check();
       this.currentUser = await this.store.api.user.me();
     } catch (e) {
       if (e.isAxiosError && e.response?.status === 401) {
@@ -75,26 +77,42 @@ export class Auth {
     this.save();
   }
 
+  public async refresh(): Promise<void> {
+    const token = await this.store.api.auth.refreshToken(this.refreshToken!);
+
+    this.accessToken = token.access_token;
+    this.refreshToken = token.refresh_token;
+
+    this.currentUser = await this.store.api.user.me();
+    this.save();
+  }
+
   public start() {
     return this.safeLoop(() => this.check());
   }
 
   private async check(): Promise<void> {
-    if (this.store.auth.accessToken === null) return;
+    if (this.accessToken === null || this.refreshToken === null) return;
 
     // todo: validate token structure
-    const token = KJUR.jws.JWS.parse(this.store.auth.accessToken).payloadObj as JWTAccessToken | undefined;
+    const token = KJUR.jws.JWS.parse(this.accessToken).payloadObj as JWTAccessToken | undefined;
+    if (!token) return;
+
+    const now = Math.floor(new Date().getTime() / 1000);
+    if (token.exp - now >= TIME_BEFORE_ISSUE_TOKEN) return;
+
+    this.refresh()
   }
 
   private async safeLoop(fn: () => Promise<void>) {
     while (this.cancelToken.token.reason === undefined) {
       try {
         await fn();
-        await new Promise(resolve => setTimeout(resolve, 5 * 1000));
+        await new Promise(resolve => setTimeout(resolve, 30 * 1000));
       } catch (e) {
         if (Axios.isCancel(e)) return;
 
-        await new Promise(resolve => setTimeout(resolve, 30 * 1000));
+        await new Promise(resolve => setTimeout(resolve, 60 * 1000));
       }
     }
   }
