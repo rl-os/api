97c24b6921e2979119e30c46a23e9ff602a1eeea
deissh <deissh@yandex.ru>
2021-04-15 08:37:47 +0300

    fix(rep/mock): tests

--- repository/mocks/mock_test.go
+++ repository/mocks/mock_test.go
@@ -15,15 +15,15 @@ func TestNewMockStore(t *testing.T) {
 		ctrl := gomock.NewController(t)
 		defer ctrl.Finish()
 
-		mocked := InitStore(ctrl)
-		mocked.BeatmapExpect().
+		mocked := NewMockBeatmap(ctrl)
+		mocked.EXPECT().
 			Get(gomock.Any(), gomock.Any()).
 			Return(&entity.SingleBeatmap{Beatmap: entity.Beatmap{ID: 123321}}, nil)
 
-		var s repository.Store
+		var s repository.Beatmap
 		s = mocked
 
-		data, err := s.Beatmap().Get(context.TODO(), 123)
+		data, err := s.Get(context.TODO(), 123)
 		assert.Nil(t, err)
 		assert.NotNil(t, data)
 		assert.Equal(t, int64(123321), data.ID)
@@ -35,15 +35,15 @@ func TestNewMockStore(t *testing.T) {
 
 		defError := errors.New("some internal errors")
 
-		mocked := InitStore(ctrl)
-		mocked.BeatmapExpect().
+		mocked := NewMockBeatmap(ctrl)
+		mocked.EXPECT().
 			Get(gomock.Any(), gomock.Any()).
 			Return(nil, defError)
 
-		var s repository.Store
+		var s repository.Beatmap
 		s = mocked
 
-		data, err := s.Beatmap().Get(context.TODO(), 1)
+		data, err := s.Get(context.TODO(), 1)
 		assert.Equal(t, err, defError)
 		assert.Nil(t, data)
 	})
